30fe7b782e6553e97bf9596b84de01ede973f48a
Mauricio Scioville <invasormou@gmail.com>
2021-03-19 09:54:45 -0500

    week 4

--- routers/posts.js
+++ routers/posts.js
@@ -1,68 +1,48 @@
 let express = require('express')
 let router = express.Router()
-
-// fake data base
-let posts = {
-  1: {
-    title: 'express.js',
-    content: 'express.js is cool',
-    author: 'some dude',
-    date: 'yesterday',
-  },
-  2: {
-    title: 'how to use express.js',
-    content: 'npm install express',
-    author: 'some dude',
-    date: 'today',
-  }
-}
-
-let n = 2
-
+let Post = require('../models/post.model')
 
 //ACTIONS GET, POST, PUT, DELETE
 
 //GET ALL THE POSTS
 router.get('/', function(req, res) {
-  res.send(posts)
+  Post.find()
+  .then(posts => res.json(posts))
+  .catch(err => res.json(err))
 })
 
 //GET ONE POST BY ID
 router.get('/:id', function(req, res) {
-  console.log('Look for post with id: ' + req.params.id)
-  let msg
-  if (posts[req.params.id]) {
-    msg = posts[req.params.id]
-  } else {
-    msg = 'no post with id: ' + req.params.id
-  }
-  res.send(msg)
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.json(err))
 })
 
 //CREATE A POST
 router.post('/', function(req, res) {
-  n += 1
-  posts[n] = {
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author,
-    date: req.body.date,
-  }
-  console.log(posts)
-  res.send('Post created successfully')
+  const { title, content, author } = req.body
+
+  const newPost = new Post({ title, content, author })
+  
+  newPost.save()
+    .then(() => res.send('Post created'))
+    .catch(err => res.json(err))
 })
 
 //UPDATE AN EXISTING POST BY ID
 router.put('/:id', function(req, res) {
-  posts[req.params.id] = posts[req.params.id] + ' !!!'
-  console.log(posts)
-  res.send(posts[req.params.id])
+  const { title, content, author } = req.body
+  
+  Post.findById(req.params.id).update({ title, content, author })
+    .then(post => res.json(post))
+    .catch(err => res.json(err))
 })
+
 //DELETE AN EXISTING POST BY ID
 router.delete('/:id', function(req, res) {
-  delete posts[req.params.id]
-  console.log(posts)
-  res.send('Post deleted')
+  Post.deleteOne({ id: req.params.id })
+    .then(() => res.send('Post deleted'))
+    .catch(err => res.json(err))
 })
 
 module.exports = router
